97a5edec0dfc8bc6e25c222a9d62473cecfbc200
hhsantos <hhsantos@gmail.com>
2025-09-12 11:08:22 +0200

    feat: Add server adapter configuration and .htaccess for URL rewriting and HTTPS enforcement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // Necesario para los endpoints API
   site: 'https://electron.finance', // Your production domain
+  adapter: node({
+    mode: 'standalone' // Crea un servidor standalone
+  })
 });
